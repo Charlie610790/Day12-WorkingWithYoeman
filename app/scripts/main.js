6bcc883668509a22f8f37714b7dff2f1b19a86db
Charlie Jones <myupstateproperties@gmail.com>
2014-05-13 13:39:56 -0400

    First two examples working.  Cleaned up class names to be easier to understand

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,46 @@
-console.log('\'Allo \'Allo!');
+// $('.example1button').hover(function() {
+//     var item = items.map(function(item) 
+//   {
+//     return item.price;
+//   })
+
+//    $(this).addClass("contenthover");
+//    $(this).text("Title Goes Here");
+//    $(this).css("font-size", $(this).val() + "30px");
+//    $(this).css("text-align", $(this).val() + "right");
+
+// });
+
+// $('.box1').mouseout(function() {
+//   $(this).text("");
+// });
+//    $(this).append('<div style="background-image: url('+ clickitem.Images[1].url_170x135 +')" class="box1"></div>')
+
+
+// EXAMPLE 1 BEGIN
+$('.example1button').click(function(clickitem) {
+	$(this).siblings('.example1').text('Throw Text On Image');
+   // $(this).css("backgound", $(this).val() + "green");
+   })
+
+$('.example1button').mouseout(function() {
+  	$(this).siblings('.example1').text('');
+});
+// EXAMPLE 1 END
+
+// EXAMPLE 2 BEGIN
+$('.example2button').click(function(clickitem) {
+	$(this).siblings('.example2').css("background", $(this).val() + "#3c4043")
+   })
+$('.example2button').mouseout(function() {
+  	$(this).siblings('.example2').css("background", $(this).val() + "#92a3b0");
+});
+// EXAMPLE 2 END
+
+// EXAMPLE 3 BEGIN
+$('.example3button').click(function(clickitem) {
+	$(this).siblings('.example3').addClass('example2extraclass');
+   });
+
+// EXAMPLE 3 END
+
